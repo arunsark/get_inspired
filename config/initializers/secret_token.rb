3d74bfdb5d96e983451932183d49b8307d43b4ba
Arun V <arun.vydianathan@gmail.com>
2011-07-20 09:53:08 +0530

    Initial Version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GetInspired::Application.config.secret_token = 'f72fbe2f604a938f33cffaeb4b5d4fd44a74ac6da57cb768edcaa9e43e67325b70d39a77bd4c7a12ecac3ef739a8e888e72467f7fcb98dfc49a207db35b20633'
